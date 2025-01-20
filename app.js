d849145d815593d06a40601ec088a8d3d3fd76f5
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-20 16:10:13 +0100

    feat: add img url

--- app.js
+++ app.js
@@ -13,31 +13,31 @@ app.get('/bacheca', (req, res) => {
         {
             title: "Introduzione a HTML: La Base del Web",
             content: "Scopri cos'è HTML, come funziona e come creare la tua prima pagina web con i tag fondamentali.",
-            image: "/img/ciambellone.jpeg",
+            image: `http://localhost:${port}/img/ciambellone.jpeg`,
             tags: ["HTML", "web development", "frontend", "tutorial"]
         },
         {
             title: "Guida Completa a CSS: Stile e Layout",
             content: "Impara come usare CSS per migliorare l'aspetto delle tue pagine web, dai colori ai layout responsivi.",
-            image: "css_guide.jpg",
+            image:  `http://localhost:${port}/img/cracker_barbabietola.jpeg`,
             tags: ["CSS", "design", "frontend", "stili"]
         },
         {
             title: "JavaScript: I Fondamenti per Principianti",
             content: "Scopri le basi di JavaScript, il linguaggio di programmazione più usato nel web moderno.",
-            image: "javascript_basics.jpg",
+            image:  `http://localhost:${port}/img/pane_fritto_dolce.jpeg`,
             tags: ["JavaScript", "frontend", "programmazione", "interattività"]
         },
         {
             title: "Introduzione a Node.js: JavaScript sul Server",
             content: "Cos'è Node.js? Scopri come eseguire JavaScript lato server e creare applicazioni veloci e scalabili.",
-            image: "nodejs_intro.jpg",
+            image:  `http://localhost:${port}/img/pasta_barbabietola.jpeg`,
             tags: ["Node.js", "backend", "JavaScript", "server"]
         },
         {
             title: "Express.js: Creare un'API REST in pochi passi",
             content: "Impara a usare Express.js per creare API RESTful e gestire le richieste HTTP in modo efficiente.",
-            image: "express_api.jpg",
+            image:  `http://localhost:${port}/img/rorta_paesana.jpeg`,
             tags: ["Express.js", "backend", "API", "Node.js"]
         }
     ])
